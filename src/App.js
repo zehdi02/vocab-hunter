6000bfeb1753bcc0f81dc91264527808ab7b9651
zehdi02 <de.melchizedek.castro@gmail.com>
2023-06-16 19:59:43 -0400

    Fixed TypeError

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import {useEffect} from 'react';
+import { useEffect } from "react";
 import "./App.css";
-import Sidebar from './components/Sidebar/Sidebar';
-import Content from './components/Content';
-import Dictionary from './components/Dictionary/Dictionary';
+import Sidebar from "./components/Sidebar/Sidebar";
+import Content from "./components/Content";
+import Dictionary from "./components/Dictionary/Dictionary";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-      <Content />
-      <Dictionary />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Sidebar />
+        <Content />
+        <Dictionary />
+      </div>
+    </BrowserRouter>
   );
 }
 
