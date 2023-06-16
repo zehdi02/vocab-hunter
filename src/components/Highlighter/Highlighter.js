becaa1edb4ac41767d609035c5fa8f525f06489f
zehdi02 <de.melchizedek.castro@gmail.com>
2023-06-16 14:58:46 -0400

    Fixed highlighters. Working perfect! imo :>

--- src/components/Highlighter/Highlighter.js
+++ src/components/Highlighter/Highlighter.js
@@ -2,67 +2,42 @@ import React, { useEffect, useState } from 'react';
 import 'highlight.js/styles/default.css';
 
 function Highlighter() {
-  // can use setAttribute("style", {bg-custom-color})
-
-  // useEffect(() => {
-  //   const handleKeyPress  = (event) => {
-  //     const key = event.key;
-  //     const word = event.target.innerText;
-  //     // console.log(word);
-
-  //     switch (key) {
-  //       case '1':
-  //         document.getElementById("wordWithoutPunctuation").setAttribute("class", 'bg-custom-red');
-  //         break;
-  //       case '2':
-  //         document.getElementById("wordWithoutPunctuation").setAttribute("class", 'bg-custom-yellow');
-  //         break;
-  //       case '3':
-  //         document.getElementById("wordWithoutPunctuation").setAttribute("class", 'bg-custom-skyblue');
-  //         break;
-  //       case '4':
-  //         document.getElementById("wordWithoutPunctuation").setAttribute("class", '');
-  //         break;
-  //       default:
-  //         break;
-  //     }
-  //   }
-
-  //   document.addEventListener('keydown', handleKeyPress);
-
-  //   return () => {
-  //     document.removeEventListener('keydown', handleKeyPress);
-  //   };
 
   useEffect(() => {
     let highlightedWord = null;
   
     const handleHighlightHover = (event) => {
-      if (event.target.tagName === 'SPAN') {
+      if (event.target.tagName === 'SPAN' && event.target.id === 'wordWithoutPunctuation') {
         const word = event.target.innerText;
         // console.log(word);
         highlightedWord = word;
       }
+      else {
+        highlightedWord = '';
+      }
     };
   
     const handleHighlightKeydown = (event) => {
-      if (highlightedWord) {
+      if (highlightedWord && document.getElementById('wordWithoutPunctuation')) {
         const words = document.getElementsByTagName('span');
-        console.log(words);
+        // console.log(words);
         for (let i = 0; i < words.length; i++) {
           const word = words[i];
           if (word.innerText === highlightedWord) {
-            if (event.key == '1') {
+            if (event.key === '1') {
+              word.classList.remove('bg-custom-yellow', 'bg-custom-skyblue');
               word.classList.add('bg-custom-red');
             }
-            if (event.key == '2') {
+            if (event.key === '2') {
+              word.classList.remove('bg-custom-red', 'bg-custom-skyblue');
               word.classList.add('bg-custom-yellow');
             }
-            if (event.key == '3') {
+            if (event.key === '3') {
+              word.classList.remove('bg-custom-red', 'bg-custom-yellow');
               word.classList.add('bg-custom-skyblue');
             }
-            if (event.key == '4') {
-              word.classList.remove('bg-custom-yellow');
+            if (event.key === '4') {
+              word.classList.remove('bg-custom-red', 'bg-custom-yellow', 'bg-custom-skyblue');
             }
           } 
         }
