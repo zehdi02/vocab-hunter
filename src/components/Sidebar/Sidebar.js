265809e579526b8d3dbb094a55d223d2ebe761c6
zehdi02 <de.melchizedek.castro@gmail.com>
2023-06-16 18:30:59 -0400

    praying merge conflicts resolved

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -75,7 +75,7 @@ function Sidebar() {
                 </a>
               </li>
               <li className="flex ">
-                <Router>
+                {/* <Router> */}
                   <Link to="/user" class="flex items-center p-2 text-gray-900 rounded-lg text-white hover:bg-gray-100 hover:bg-gray-700">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -85,7 +85,7 @@ function Sidebar() {
                   <Routes>
                     <Route path="/user" element={<User />} />
                   </Routes>
-                </Router>
+                {/* </Router> */}
               </li>
 
             </ul>
