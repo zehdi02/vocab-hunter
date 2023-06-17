7e215ca4891aa32eabc37348f05ab127e38368a2
zehdi02 <de.melchizedek.castro@gmail.com>
2023-06-17 13:03:31 -0400

    Built Vocabulary page

--- src/components/Sidebar/Pages/Vocabulary.js
+++ src/components/Sidebar/Pages/Vocabulary.js
@@ -1,14 +1,87 @@
 import React from "react";
+import './Vocabulary.css'
 
 function Vocabulary() {
   return (
-    <div className="Vocab h-screen">
-      <div className="container">
-        <div className="title">Vocabulary</div>
-        <div className="container2">
-          <div className="known">KNOWN</div>
-          <div className="unknown">UNKNOWN</div>
-        </div>
+    <div className="Vocab flex flex-col
+    h-fit text-gray-200 bg-gray-800 rounded-xl px-8 py-4
+    ">
+      <div className="title text-2xl font-bold text-center 
+      flex-initial">
+        Your Vocabulary
+      </div>      
+      <div className="wordbank px-2 py-4 flex-1">
+      <table className="table-fixed border border-gray-200 rounded-full">
+        <thead className="text-xl border">
+          <tr className="known-bank">
+            <th class='w-2/4'>Known Words: <span>20,000</span></th>
+            <th class='w-2/4'>Unknown Words: <span>500</span></th>
+          </tr>
+        </thead>
+        <tbody className="">
+          <tr><td>label1</td><td>value1</td></tr>
+          <tr><td>label2</td><td>value2</td></tr>
+          <tr><td>label3</td><td>value3</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+          <tr><td>label4</td><td>value4</td></tr>
+        </tbody>
+      </table>
       </div>
     </div>
   );
